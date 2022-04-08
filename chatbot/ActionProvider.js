305c432509ca63fd244a0db1f72b371bc4906be5
Abhishek Asarawa <100663337+abhishek-eubrics@users.noreply.github.com>
2022-04-08 10:47:13 +0530

    Setting chatbot

---
+++ chatbot/ActionProvider.js
@@ -0,0 +1,9 @@
+class ActionProvider {
+    constructor(createChatBotMessage, setStateFunc, createClientMessage) {
+        this.createChatBotMessage = createChatBotMessage;
+        this.setState = setStateFunc;
+        this.createClientMessage = createChatBotMessage;
+    }
+}
+
+export default ActionProvider;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Chatbot from "react-chatbot-kit";
+import ActionProvider from "../chatbot/ActionProvider";
+import config from "../chatbot/config";
+import MessageParser from "../chatbot/MessageParser";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Component {...pageProps} />
+            <Chatbot
+                config={config}
+                messageParser={MessageParser}
+                actionProvider={ActionProvider}
+            />
+        </div>
+    );
 }
 
-export default MyApp
+export default MyApp;
